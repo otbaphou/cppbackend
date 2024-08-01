3f4f3d1d97f8558d1029ea27a863fe61f02eab19
0tba <72134530+otbaphou@users.noreply.github.com>
2024-08-02 01:46:11 +0300

    Update request_handler.h

--- sprint1/problems/final_task/solution/src/request_handler.h
+++ sprint1/problems/final_task/solution/src/request_handler.h
@@ -26,6 +26,13 @@ namespace http_handler
 	StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version, 
 	bool keep_alive, std::string_view content_type = ContentType::APPLICATION_JSON);
 
+	//Data Packets
+	void PackMaps(json::array& target_container, model::Game& game);
+
+	void PackRoads(json::array& target_container, const model::Map* map_ptr);
+	void PackBuildings(json::array& target_container, const model::Map* map_ptr);
+	void PackOffices(json::array& target_container, const model::Map* map_ptr);
+
 	StringResponse HandleRequest(auto&& req, model::Game& game)
 	{
 
@@ -47,23 +54,15 @@ namespace http_handler
 
 		if (size >= 4)
 		{
-			json::array response;
 
 			if (std::string_view(target.begin(), target.begin() + 5) == "/api/"sv)
 			{
 				if (target == "/api/v1/maps"sv || target == "/api/v1/maps/"sv)
 				{
-					//Sends Off JSON Dictionary With Maps
-					
-					for (auto& map : game.GetMaps())
-					{
-						json::object elem;
+					json::array response;
 
-						elem.emplace("id", *map.GetId());
-						elem.emplace("name", map.GetName());
-
-						response.push_back(elem);
-					}
+					//Inserting Map Data Into JSON Array
+					PackMaps(response, game);
 
 					return text_response(http::status::ok, { json::serialize(response) });
 				}
@@ -80,115 +79,60 @@ namespace http_handler
 						if (maptr != nullptr)
 						{
 							//JSON object with map data
-							json::object elem;
+							json::object response;
 
 							//Initializing object using map id and name
-							elem.emplace("id", *maptr->GetId());
-							elem.emplace("name", maptr->GetName());
+							response.emplace("id", *maptr->GetId());
+							response.emplace("name", maptr->GetName());
 
-							//Road container
+							//Inserting Roads
 							json::array roads;
+							PackRoads(roads, maptr);
 
-							for (const model::Road& road_data : maptr->GetRoads())
-							{
-								//Temporary road object
-								json::object road;
+							response.emplace("roads", std::move(roads));
 
-								model::Point start = road_data.GetStart();
-								model::Point end = road_data.GetEnd();
-
-								road.emplace("x0", start.x);
-								road.emplace("y0", start.y);
-
-								if (road_data.IsHorizontal())
-								{
-									road.emplace("x1", end.x);
-								}
-								else
-								{
-									road.emplace("y1", end.y);
-								}
-
-								//Pushing road to the road container
-								roads.push_back(road);
-							}
-
-							//Inserting road data to the map object
-							elem.emplace("roads", roads);
-
-							//Building container
+							//Inserting Buildings
 							json::array buildings;
+							PackBuildings(buildings, maptr);
 
-							for (const model::Building& building_data : maptr->GetBuildings())
-							{
-								//Temporary building object
-								json::object building;
+							response.emplace("buildings", std::move(buildings));
 
-								model::Rectangle dimensions = building_data.GetBounds();
-
-								building.emplace("x", dimensions.position.x);
-								building.emplace("y", dimensions.position.y);
-								building.emplace("w", dimensions.size.width);
-								building.emplace("h", dimensions.size.height);
-
-								//Pushing building to the building container
-								buildings.push_back(building);
-							}
-
-							//Inserting building data to the map object
-							elem.emplace("buildings", buildings);
-
-							//Office container
+							//Inserting Offices
 							json::array offices;
+							PackOffices(offices, maptr);
 
-							for (const model::Office& office_data : maptr->GetOffices())
-							{
-								//Temporary office object
-								json::object office;
+							response.emplace("offices", std::move(offices));
 
-								office.emplace("id", *office_data.GetId());
-
-								model::Point pos = office_data.GetPosition();
-								office.emplace("x", pos.x);
-								office.emplace("y", pos.y);
-
-								model::Offset offset = office_data.GetOffset();
-								office.emplace("offsetX", offset.dx);
-								office.emplace("offsetY", offset.dy);
-
-								//Pushing office to the building container
-								offices.push_back(office);
-							}
-
-							//Inserting office data to the map object
-							elem.emplace("offices", offices);
-
-							//Printing elem instead of response :o
-							return text_response(http::status::ok, { json::serialize(elem) });
+							return text_response(http::status::ok, { json::serialize(response) });
 						}
 						else
 						{
 							//Throwing error if requested map isn't found
 
-							json::object elem;
+							json::object response;
 
-							elem.emplace("code", "mapNotFound");
-							elem.emplace("message", "Map not found");
+							response.emplace("code", "mapNotFound");
+							response.emplace("message", "Map not found");
 
-							return text_response(http::status::not_found, { json::serialize(elem) });
+							return text_response(http::status::not_found, { json::serialize(response) });
 						}
 					}
 				}
+				//Throwing error when URL starts with /api/ but doesn't correlate to any of the commands
 
+				json::object elem;
+
+				elem.emplace("code", "badRequest");
+				elem.emplace("message", "Bad request");
 			}				
 			//Throwing error when URL starts with /api/ but doesn't correlate to any of the commands
 
-			json::object elem;
+			json::object response;
 
-			elem.emplace("code", "badRequest");
-			elem.emplace("message", "Bad request");
+			response.emplace("code", "badRequest");
+			response.emplace("message", "Bad request");
 
-			return text_response(http::status::bad_request, { json::serialize(elem) });
+			return text_response(http::status::bad_request, { json::serialize(response) });
 		}
 
 		std::string response_str = "Why Are We Here?";
