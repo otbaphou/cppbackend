20ddf857c29461f8c40705f8b8b210648b8bfb74
0tba <72134530+otbaphou@users.noreply.github.com>
2024-08-02 01:37:27 +0300

    Update json_loader.h

--- sprint1/problems/final_task/solution/src/json_loader.h
+++ sprint1/problems/final_task/solution/src/json_loader.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <filesystem>
+#include <string>
 
 #include "model.h"
 
 namespace json_loader 
 {
+	void ParseRoads(model::Map& map, json::object& parent_object);
+	void ParseBuildings(model::Map& map, json::object& parent_object);
+	void ParseOffices(model::Map& map, json::object& parent_object);
 
 	model::Game LoadGame(const std::filesystem::path& json_path);
 
