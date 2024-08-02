ae472397462c06a998a1fe7bc9e36af1c30f3617
0tba <72134530+otbaphou@users.noreply.github.com>
2024-08-02 06:07:22 +0300

    Update my_logger.h

--- sprint2/problems/logger/solution/my_logger.h
+++ sprint2/problems/logger/solution/my_logger.h
@@ -11,6 +11,7 @@
 #include <filesystem>
 #include <mutex>
 #include <thread>
+#include <syncstream>
 
 using namespace std::literals;
 
@@ -38,13 +39,15 @@ class Logger
     // Для имени файла возьмите дату с форматом "%Y_%m_%d"
     std::string GetFileTimeStamp() const
     {
-        const std::chrono::time_point now{ std::chrono::system_clock::now() };
-        const std::chrono::year_month_day ymd{ std::chrono::floor<std::chrono::days>(now) };
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
 
-        std::stringstream date; 
-        date << ymd;
+        char mbstr[100];
+        std::strftime(mbstr, 100, "%Y_%m_%d", std::localtime(&t_c));
 
-        return date.str();
+        std::string time(mbstr);
+
+        return time;
     }
 
     Logger() = default;
@@ -57,21 +60,38 @@ public:
         return obj;
     }
 
+    void SetPath()
+    {
+        std::string raw_path = "/var/log/";
+        raw_path += current_date;
+        raw_path += ".log";
+
+        std::filesystem::path tmpath{ raw_path };
+        current_path = std::move(tmpath);
+
+        log_file_.close();
+
+        log_file_ = std::ofstream{current_path, std::ios::app };
+        log_file_.open(current_path);
+    }
+
     // Выведите в поток все аргументы.
     template<class... Ts>
     void Log(const Ts&... args)
     {
-        ///var/log/
-        std::string raw_path = "C:/Users/0tba/Desktop/";
-        raw_path += GetFileTimeStamp();
-        raw_path += ".log";
+        std::string current_timestamp = GetFileTimeStamp();
 
-        std::filesystem::path log_path{ raw_path };
-        std::ofstream log_file_{ log_path, std::ios::app };
+        if (current_date != current_timestamp || !log_file_.is_open())
+        {
+            current_date = std::move(current_timestamp);
+            SetPath();
+        }
 
-        log_file_ << GetTimeStamp() << ": "sv;
-        ((log_file_ << args), ...);
-        log_file_ << std::endl;
+        std::osyncstream stream {log_file_};
+
+        stream << GetTimeStamp() << ": "sv;
+        ((stream << args), ...);
+        stream << std::endl;
     }
 
     // Установите manual_ts_. Учтите, что эта операция может выполняться
@@ -84,5 +104,10 @@ public:
     }
 
 private:
+
+    std::string current_date = "";
+    std::filesystem::path current_path = "";
+    std::ofstream log_file_;
+
     std::optional<std::chrono::system_clock::time_point> manual_ts_;
 };
