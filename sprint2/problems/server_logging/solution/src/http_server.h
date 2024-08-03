1ef92abb763c707e3f3b4b67888eae1a74c6553b
0tba <72134530+otbaphou@users.noreply.github.com>
2024-08-03 09:54:04 +0300

    Add files via upload

--- sprint2/problems/server_logging/solution/src/http_server.h
+++ sprint2/problems/server_logging/solution/src/http_server.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <boost/log/trivial.hpp>
 #include <boost/log/core.hpp>        // для logging::core
-#include <boost/log/expressions.hpp>
+#include <boost/log/expressions.hpp> // для выражения, задающего фильтр
 #include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/manipulators/add_value.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/date_time.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
 
 #include <boost/json.hpp>
 
