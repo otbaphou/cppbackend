da151370b6435ab8d8b3b9d52ec79276fbf21dd5
0tba <72134530+otbaphou@users.noreply.github.com>
2024-08-04 08:46:51 +0300

    Add files via upload

---
+++ sprint2/problems/game_state/solution/src/json_loader.cpp
@@ -0,0 +1,132 @@
+#include "json_loader.h"
+#include "tagged.h"
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+namespace json_loader
+{
+
+	void ParseRoads(model::Map& map, json::object& parent_object)
+	{
+		for (auto road_entry : parent_object.at("roads").as_array())
+		{
+			json::object road_data = road_entry.as_object();
+
+			if (road_data.contains(X1_COORDINATE))
+			{
+				model::Road road(
+					{
+						model::Road::HORIZONTAL, 
+						{static_cast<int>(road_data.at(X0_COORDINATE).as_int64()), static_cast<int>(road_data.at(Y0_COORDINATE).as_int64())},
+						static_cast<int>(road_data.at(X1_COORDINATE).as_int64())
+					});
+
+				map.AddRoad(std::move(road));
+			}
+			else
+			{
+				model::Road road(
+					{
+						model::Road::VERTICAL,
+						{static_cast<int>(road_data.at(X0_COORDINATE).as_int64()), static_cast<int>(road_data.at(Y0_COORDINATE).as_int64())},
+						static_cast<int>(road_data.at(Y1_COORDINATE).as_int64())
+					});
+
+				map.AddRoad(std::move(road));
+			}
+
+		}
+	}
+
+
+	void ParseBuildings(model::Map& map, json::object& parent_object)
+	{
+		for (auto building_entry : parent_object.at("buildings").as_array())
+		{
+			json::object building_data = building_entry.as_object();
+
+			model::Building building(
+				{
+					{static_cast<int>(building_data.at(X_COORDINATE).as_int64()), static_cast<int>(building_data.at(Y_COORDINATE).as_int64())},
+					{static_cast<int>(building_data.at(WIDTH).as_int64()), static_cast<int>(building_data.at(HEIGHT).as_int64())}
+				});
+
+			map.AddBuilding(std::move(building));
+		}
+	}
+
+	void ParseOffices(model::Map& map, json::object& parent_object)
+	{
+		for (auto office_entry : parent_object.at("offices").as_array())
+		{
+			json::object office_data = office_entry.as_object();
+
+			using Id = util::Tagged<std::string, model::Office>;
+			Id id{ std::string(office_data.at("id").as_string()) };
+
+			model::Office office(
+				{
+					id,
+					{static_cast<int>(office_data.at(X_COORDINATE).as_int64()), static_cast<int>(office_data.at(Y_COORDINATE).as_int64())},
+					{static_cast<int>(office_data.at(X_OFFSET).as_int64()), static_cast<int>(office_data.at(Y_OFFSET).as_int64())}
+				});
+
+			map.AddOffice(std::move(office));
+		}
+	}
+
+	model::Game LoadGame(const std::filesystem::path& json_path)
+	{
+		// Загрузить содержимое файла json_path, например, в виде строки
+		// Распарсить строку как JSON, используя boost::json::parse
+		// Загрузить модель игры из файла
+		model::Game game;
+
+		std::ifstream file(json_path);
+
+		if (!file.is_open())
+		{
+			json::object tmp{ {"error loading file", json_path.string()} };
+			json::object logger_data{ {"code", -31257}, {"exception", tmp} };
+
+			BOOST_LOG_TRIVIAL(info) << logging::add_value(timestamp, pt::microsec_clock::local_time()) << logging::add_value(additional_data, logger_data) << "error";
+			throw std::invalid_argument("Loading Error! Game data file couldn't be opened..");
+		}
+
+		std::ostringstream tmp;
+
+		tmp << file.rdbuf();
+
+		std::string json_str = tmp.str();
+
+		auto value = json::parse(json_str);
+
+		json::array maps = value.as_object().at("maps").as_array();
+
+		for (auto entry : maps)
+		{
+			//Unpacking JSON object that contains data for the current map
+			json::object map_data = entry.as_object();
+
+
+			using Id = util::Tagged<std::string, model::Map>;
+			Id id{ std::string(map_data.at("id").as_string()) };
+
+			//Initializing map
+			model::Map map(id, std::string(map_data.at("name").as_string()));
+
+			//Reading JSON-object containing roads from map_data and adding them to the map
+			ParseRoads(map, map_data);
+			//Doing the same with buildings and offices
+			ParseBuildings(map, map_data);
+			ParseOffices(map, map_data);
+
+			//Adding the map to the game
+			game.AddMap(map);
+		}
+
+		return game;
+	}
+
+}  // namespace json_loader
---
+++ sprint2/problems/game_state/solution/src/main.cpp
@@ -0,0 +1,126 @@
+#include "http_server.h"
+#include "sdk.h"
+
+#include <boost/asio/io_context.hpp>
+#include <iostream>
+#include <thread>
+
+#include "json_loader.h"
+#include "request_handler.h"
+
+using namespace std::literals;
+namespace net = boost::asio;
+namespace sys = boost::system;
+namespace http = boost::beast::http;
+
+namespace
+{
+	// Запускает функцию fn на n потоках, включая текущий
+	template <typename Fn>
+	void RunWorkers(unsigned n, const Fn& fn)
+	{
+		n = std::max(1u, n);
+		std::vector<std::jthread> workers;
+		workers.reserve(n - 1);
+		// Запускаем n-1 рабочих потоков, выполняющих функцию fn
+		while (--n) {
+			workers.emplace_back(fn);
+		}
+		fn();
+	}
+
+}  // namespace
+
+//Once again, I don't know where else to place this stuff below. This file seemed suitable though..
+
+void MyFormatter(logging::record_view const& rec, logging::formatting_ostream& strm)
+{
+	json::object final_obj;
+	//Parsing timestamp and inserting it into the final json::object
+	final_obj.emplace("timestamp", pt::to_iso_extended_string(rec[timestamp].get()));
+
+	//Inserting additional information into the final json::object
+	final_obj.emplace("data", rec[additional_data].get());
+
+	//Inserting message string into the final json::object
+	final_obj.emplace("message", rec[logging::expressions::smessage].get());
+
+	strm << json::serialize(final_obj);
+}
+
+void InitBoostLogFilter()
+{
+	logging::add_console_log(
+		std::cout,
+		keywords::format = &MyFormatter,
+		keywords::auto_flush = true
+	);
+
+	/*logging::add_file_log(
+		keywords::file_name = "game_server.log",
+		keywords::format = &MyFormatter,
+		keywords::open_mode = std::ios_base::app | std::ios_base::out
+	);*/
+}
+
+int main(int argc, const char* argv[])
+{
+	if (argc != 3)
+	{
+		std::cerr << "Usage: game_server <game-config-json> <static-files>"sv << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		// 1. Загружаем карту из файла и построить модель игры
+		model::Game game = json_loader::LoadGame(argv[1]);
+
+		// 2. Инициализируем io_context
+		const unsigned num_threads = std::thread::hardware_concurrency();
+		net::io_context ioc(num_threads);
+
+		// 3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
+
+		// 4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
+		http_handler::RequestHandler handler{ argv[2], game };
+
+		const auto address = net::ip::make_address("0.0.0.0");
+		constexpr net::ip::port_type port = 8080;
+
+		// 5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
+
+		InitBoostLogFilter();
+
+		http_server::ServeHttp(ioc, { address, port }, [&handler](auto&& req, auto&& send)
+			{
+				handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+			});
+
+		// Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
+		json::object logger_data{ {"port", static_cast<unsigned>(port)}, {"address", address.to_string()} };
+
+		BOOST_LOG_TRIVIAL(info) << logging::add_value(timestamp, pt::microsec_clock::local_time()) << logging::add_value(additional_data, logger_data) << "Server has started..."sv;
+
+		// 6. Запускаем обработку асинхронных операций
+		RunWorkers(std::max(1u, num_threads), [&ioc]
+			{
+				ioc.run();
+			});
+	}
+
+	catch (const std::exception& ex)
+	{
+		//Logging server exit with errors
+		json::object logger_data{ {"code", EXIT_FAILURE}, {"exception", ex.what()} };
+
+		BOOST_LOG_TRIVIAL(info) << logging::add_value(timestamp, pt::microsec_clock::local_time()) << logging::add_value(additional_data, logger_data) << "server exited"sv;
+
+		return EXIT_FAILURE;
+	}
+
+	//Logging server exit without errors
+	json::object logger_data{ {"code", 0} };
+
+	BOOST_LOG_TRIVIAL(info) << logging::add_value(timestamp, pt::microsec_clock::local_time()) << logging::add_value(additional_data, logger_data) << "server exited"sv;
+}
