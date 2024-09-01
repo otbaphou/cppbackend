e9c3acfeb062db5e9441bd338e4b783d8bd83cf7
0tba <72134530+otbaphou@users.noreply.github.com>
2024-09-01 22:58:17 +0300

    Add files via upload

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -48,9 +48,9 @@ def make_shots():
 
 
 server = run(start_server())
-subprocess.Popen(shlex.split("sudo perf record -o perf.data -p" + str(server.pid)), stdout=subprocess.PIPE)
+subprocess.Popen(shlex.split("sudo perf record -g -o perf.data -p" + str(server.pid)), stdout=subprocess.PIPE)
 make_shots()
 stop(server)
 time.sleep(1)
-subprocess.Popen('sudo perf script  -f -i perf.data | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl > graph.svg',  stdout=None, shell=True)
+subprocess.Popen('sudo perf script -f -i perf.data | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl > graph.svg',  stdout=None, shell=True)
 print('Job done')
