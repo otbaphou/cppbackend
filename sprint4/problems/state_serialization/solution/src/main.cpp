d8e4ddcc6e736fa2cf24b5c8e51e359101a4ae80
0tba <72134530+otbaphou@users.noreply.github.com>
2024-09-07 07:20:31 +0300

    Update main.cpp

--- sprint4/problems/state_serialization/solution/src/main.cpp
+++ sprint4/problems/state_serialization/solution/src/main.cpp
@@ -237,15 +237,12 @@ int main(int argc, const char* argv[])
 		const unsigned num_threads = std::thread::hardware_concurrency();
 		net::io_context ioc(num_threads);
 
-		bool terminated = false;
-
-		net::signal_set signals(ioc, SIGINT, SIGTERM);
-		signals.async_wait([&ioc, &args, &save_manager, &terminated](const sys::error_code& ec, [[maybe_unused]] int signal_number)
+		net::signal_set signals(ioc, SIGINT);
+		signals.async_wait([&ioc, &args, &save_manager](const sys::error_code& ec, [[maybe_unused]] int signal_number)
 			{
 				if (!ec)
 				{
 					ioc.stop();
-					terminated = true;
 				}
 			});
 
@@ -306,7 +303,7 @@ int main(int argc, const char* argv[])
 				ioc.run();
 			});
 
-		if (!args.save_file.empty() && terminated == false)
+		if (!args.save_file.empty())
 		{
 			save_manager.SaveState();
 		}
