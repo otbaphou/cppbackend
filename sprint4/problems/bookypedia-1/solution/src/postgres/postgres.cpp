2dd81a5244a5fbab214d0df279b6119fee0f5b37
0tba <72134530+otbaphou@users.noreply.github.com>
2024-09-14 14:46:05 +0300

    Update postgres.cpp

--- sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -14,7 +14,7 @@ namespace postgres {
 		// В будущих уроках вы узнаете про паттерн Unit of Work, при помощи которого сможете несколько
 		// запросов выполнить в рамках одной транзакции.
 		// Вы также может самостоятельно почитать информацию про этот паттерн и применить его здесь.
-		pqxx::transaction<pqxx::isolation_level::read_committed> work{ connection_ };
+		pqxx::work work{ connection_ };
 		work.exec_params(R"(INSERT INTO authors (id, name) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET name=$2;)"_zv, author.GetId().ToString(), author.GetName());
 		work.commit();
 	}
@@ -43,7 +43,7 @@ namespace postgres {
 
 	void BookRepositoryImpl::Save(const domain::Book& book)
 	{
-		pqxx::transaction<pqxx::isolation_level::read_committed> work{ connection_ };
+		pqxx::work work{ connection_ };
 		work.exec_params(R"(INSERT INTO books (id, author_id, title, publication_year) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET author_id=$2, title=$3, publication_year=$4;)"_zv, 
 			book.GetId().ToString(), book.GetAuthorId().ToString(), book.GetName(), book.GetReleaseYear());
 		work.commit();
@@ -97,7 +97,7 @@ namespace postgres {
 	Database::Database(pqxx::connection connection)
 		: connection_{ std::move(connection) }
 	{
-		pqxx::transaction<pqxx::isolation_level::read_committed> work{ connection_ };
+		pqxx::work work{ connection_ };
 		work.exec(R"(CREATE TABLE IF NOT EXISTS authors ( id UUID CONSTRAINT author_id_constraint PRIMARY KEY, name varchar(100) UNIQUE NOT NULL );)"_zv);
 		work.exec(R"(CREATE TABLE IF NOT EXISTS books ( id UUID CONSTRAINT book_id_constraint PRIMARY KEY, author_id UUID NOT NULL, title varchar(100) NOT NULL, publication_year integer);)"_zv);
 
