0afe9469ddc984d25555ed7926850cd5e0706903
0tba <72134530+otbaphou@users.noreply.github.com>
2024-09-18 02:44:42 +0300

    Add files via upload

--- sprint4/problems/bookypedia-2/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-2/solution/src/postgres/postgres.cpp
@@ -311,7 +311,7 @@ namespace postgres {
 		pqxx::work work{ connection_ };
 		work.exec(R"(CREATE TABLE IF NOT EXISTS authors ( id UUID CONSTRAINT author_id_constraint PRIMARY KEY, name varchar(100) UNIQUE NOT NULL );)"_zv);
 		work.exec(R"(CREATE TABLE IF NOT EXISTS books ( id UUID CONSTRAINT book_id_constraint PRIMARY KEY, author_id UUID NOT NULL, title varchar(100) NOT NULL, publication_year integer);)"_zv);
-		work.exec(R"(CREATE TABLE IF NOT EXISTS book_tags ( book_id UUID CONSTRAINT book_id_constraint PRIMARY KEY, tag varchar(30) UNIQUE);)"_zv);
+		work.exec(R"(CREATE TABLE IF NOT EXISTS book_tags ( book_id UUID CONSTRAINT book_t_id_constraint PRIMARY KEY, tag varchar(30) UNIQUE);)"_zv);
 
 		// ... создать другие таблицы
 
