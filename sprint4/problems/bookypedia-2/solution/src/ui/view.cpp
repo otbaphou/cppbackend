9c3de64df6d33e57c9d4ec72934ec23ad4f39b62
0tba <72134530+otbaphou@users.noreply.github.com>
2024-09-19 01:13:10 +0300

    Update view.cpp

--- sprint4/problems/bookypedia-2/solution/src/ui/view.cpp
+++ sprint4/problems/bookypedia-2/solution/src/ui/view.cpp
@@ -129,7 +129,7 @@ namespace ui {
         menu_.AddAction("DeleteBook"s, {}, "Deletes book"s, std::bind(&View::DeleteBook, this, ph::_1));
         menu_.AddAction("DeleteAuthor"s, {}, "Deletes author"s, std::bind(&View::DeleteAuthor, this, ph::_1));
         menu_.AddAction("EditBook"s, {}, "Edits book"s, std::bind(&View::EditBook, this, ph::_1));
-        menu_.AddAction("EditAuthor"s, "<title>"s, "Edits author"s, std::bind(&View::EditAuthor, this, ph::_1));
+        menu_.AddAction("EditAuthor"s, {}, "Edits author"s, std::bind(&View::EditAuthor, this, ph::_1));
         menu_.AddAction("ShowAuthors"s, {}, "Show authors"s, std::bind(&View::ShowAuthors, this));
         menu_.AddAction("ShowBooks"s, {}, "Show books"s, std::bind(&View::ShowBooks, this));
         menu_.AddAction("ShowBook"s, {}, "Shows book"s, std::bind(&View::ShowBook, this, ph::_1));
@@ -161,57 +161,57 @@ namespace ui {
 
     bool View::AddBook(std::istream& cmd_input) const
     {
-        //try
-        //{
-        //    if (auto params = GetBookParams(cmd_input))
-        //    {
-        //        if (!params.has_value())
-        //        {
-        //            //throw std::invalid_argument("Failed to add the book..");
-        //            return true;
-        //        }
+        try
+        {
+            if (auto params = GetBookParams(cmd_input))
+            {
+                if (!params.has_value())
+                {
+                    //throw std::invalid_argument("Failed to add the book..");
+                    return true;
+                }
 
-        //        detail::AddBookParams p = params.value();
-        //        
-        //        if (!p.title.empty())
-        //        {
-        //            use_cases_.AddBook(p.publication_year, p.title, p.author_id);
+                detail::AddBookParams p = params.value();
+                
+                if (!p.title.empty())
+                {
+                    use_cases_.AddBook(p.publication_year, p.title, p.author_id);
 
-        //            output_ << "Enter tags (comma separated):\n";
+                    output_ << "Enter tags (comma separated):\n";
 
-        //            std::string tags;
-        //            std::getline(cmd_input, tags);
-        //            boost::algorithm::trim(tags);
+                    std::string tags;
+                    std::getline(cmd_input, tags);
+                    boost::algorithm::trim(tags);
 
-        //            auto& tmp_books = use_cases_.GetBooksWithName(p.title);
+                    auto& tmp_books = use_cases_.GetBooksWithName(p.title);
 
-        //            std::string current_id;
+                    std::string current_id;
 
-        //            if (tmp_books.size() > 1)
-        //            {
-        //                for (auto& book : tmp_books)
-        //                {
-        //                    if (book.author_id.ToString() == p.author_id)
-        //                    {
-        //                        current_id = book.book_id.ToString();
-        //                    }
-        //                }
-        //            }
+                    if (tmp_books.size() > 1)
+                    {
+                        for (auto& book : tmp_books)
+                        {
+                            if (book.author_id.ToString() == p.author_id)
+                            {
+                                current_id = book.book_id.ToString();
+                            }
+                        }
+                    }
 
-        //            if(!current_id.empty())
-        //            {
-        //                use_cases_.SaveTags(current_id, ParseTags(tags));
-        //            }
-        //        }
+                    if(!current_id.empty())
+                    {
+                        use_cases_.SaveTags(current_id, ParseTags(tags));
+                    }
+                }
 
-        //        return true;
-        //    }
-        //}
-        //catch (const std::exception& ex)
-        //{
-        //    output_ << "Failed to add book"sv << std::endl;
-        //}
-        //return true;
+                return true;
+            }
+        }
+        catch (const std::exception& ex)
+        {
+            output_ << "Failed to add book"sv << std::endl;
+        }
+        return true;
     }
 
     bool View::DeleteBook(std::istream& cmd_input) const
@@ -341,22 +341,17 @@ namespace ui {
                 else
                 {
                     author_id = author.value();
-
-                    if (author_id == "")
-                    {
-                        return true;
-                    }
                 }
             }
             else
             {
                 author_id = use_cases_.GetAuthorId(name);
+            }
 
-                if (author_id == "")
-                {
-                    throw std::invalid_argument("Author not found!!\n");
-                    //return true;
-                }
+            if (author_id == "")
+            {
+                throw std::invalid_argument("Author not found!!\n");
+                //return true;
             }
 
             output_ << "Enter new name:\n";
